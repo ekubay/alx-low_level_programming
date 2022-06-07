ead80cb7eef4af9668948a73d340b59c84fb97ad
ekubay <axuTec14@gmail.com>
2022-06-07 09:38:39 -0700

    prints signs of the imputs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - determine either it is +ve, *ve or zero
+ * @n: the number to be checked
+ * Return: 1 if posotive, 0 if negative and -1 otherwise
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('-');
+		return ('/');
+	}
+}
