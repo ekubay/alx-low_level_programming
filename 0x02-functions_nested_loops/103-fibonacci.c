4821b6923ee7e06058ef90bebc1215da4532dbcf
ekubay <axuTec14@gmail.com>
2022-06-08 02:37:36 -0700

    prints the sum of even fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even fibonacci sequence
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long fb1 = 0, fb2 = 1, fsum = 0;
+	float totalsum = 0;
+
+	while (1)
+	{
+		fsum = fb1 + fb2;
+		if (fsum > 400000)
+			break;
+		if ((fsum % 2) == 0)
+			totalsum += fsum;
+
+		fb1 = fb2;
+		fb2 = fsum;
+	}
+	printf("%.0f\n", totalsum);
+	return (0);
+}
