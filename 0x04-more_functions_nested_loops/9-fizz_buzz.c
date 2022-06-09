ecf1b49d834cae1490b6f356154ace51ff8366a5
ekubay <axuTec14@gmail.com>
2022-06-09 06:15:17 -0700

    printing multiplies of 3 and 4

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numberfrom 1 to 100
+ * but for multiplies of 3 print Fizz
+ * multiplies of 5 print Buzz, for numbers wh are multiplies
+ * both print FizzBuzz
+ *
+ * Return: always success
+ */
+int main(void)
+{
+	int i = 1;
+
+	for (; i < 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("Buzz\n");
+}
