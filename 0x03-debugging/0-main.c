8fff9f81716562dd2e15d149e20bc03a65ae6b13
ekubay <axuTec14@gmail.com>
2022-06-09 02:25:44 -0700

    +ve, -ve or zero

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include "main.h"
+
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
@@ -12,10 +10,6 @@ int main(void)
 	int i;
 
 	i = 0;
-	/*
-	 * srand(time(0));
-	 * n = rand() - RAND_MAX / 2;
-	 */
 	positive_or_negative(i);
 
 	return (0);
