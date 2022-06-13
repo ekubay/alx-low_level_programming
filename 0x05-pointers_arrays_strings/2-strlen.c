cd36093c9850aae287ca16cf1d525fd330d0f8da
ekubay <axuTec14@gmail.com>
2022-06-13 06:55:14 +0000

    prints the length of the string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - prints the length of string
+ * @s: pointer to string
+ * Return: always success
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
