aed2c97116eca75edd27d391becf42385c6d6c8c
ekubay <axuTec14@gmail.com>
2022-06-13 08:29:20 +0000

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strcpy - copies string
+ * @dest: char type string, destination
+ * @src: char type string, source
+ * Return: always success
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	do {
+		dest[i] = src[i];
+		i++;
+	} while (src[i] != '\0');
+
+	return (dest);
+}
