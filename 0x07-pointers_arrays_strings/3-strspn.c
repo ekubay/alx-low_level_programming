a2770c62f273fb6f9704f61e3a8e32f6753c7ebf
ekubay <axuTec14@gmail.com>
2022-06-20 12:22:26 +0000

    prints length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strspn - gets length of the  prifix substring
+ * @s: segment to compare bytes
+ * @accept: sting of byte to compare
+ * Return: always success
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0' && s[i] != accept[j])
+			j++;
+		while (accept[j] == '\0')
+			return (i);
+		i++;
+	}
+	return (i);
+}
