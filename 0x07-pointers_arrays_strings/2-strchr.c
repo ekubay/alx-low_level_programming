c2cbb03c38d7467863cdb63c6e45b2e44f1713d8
ekubay <axuTec14@gmail.com>
2022-06-20 14:32:17 +0000

    locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,15 +7,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	if (*s != '\0')
+	while (*s != '\0')
 	{
 		if (*s == c)
 		{
 			return (s);
 		}
-		else if (*(s + 1) == c)
-			return (s + 1);
 		s++;
 	}
-	return (s + 1);
+	if (*s == c)
+		return (s);
+	return (0);
 }
